72a03f9453e61fb2e6c1ce135a57e6a4e35335bc
Sander Mertens <sander.mertens8@gmail.com>
2019-02-10 02:21:55 -0800

    Initial commit

---
+++ include/example_package.h
@@ -0,0 +1,8 @@
+#ifndef EXAMPLE_PACKAGE_H
+#define EXAMPLE_PACKAGE_H
+
+/* This generated file contains includes for project dependencies */
+#include "bake_config.h"
+
+#endif
+
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <include/example_package.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
